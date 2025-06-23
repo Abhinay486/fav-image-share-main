27eb501bd6eb6c7ae25b74e78f706f86d660f2b1
Kollimarla Nrusimha Sri Ganesh <116094202+sri-ganeshk@users.noreply.github.com>
2025-06-23 14:05:28 +0000

    gg

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -8,8 +8,7 @@ const generateToken = (id, res) => {
     res.cookie("token", token, {
         maxAge: 15 * 24 * 60 * 60 * 1000, // Convert days to milliseconds
         httpOnly: true, // Prevent client-side access
-        sameSite: "strict", // Prevent CSRF attacks
-        secure: process.env.NODE_ENV === "production", // Secure cookie in production
+        sameSite: "none", // Prevent CSRF attacks
     });
 };
 
